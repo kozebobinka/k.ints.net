9e316516b818ef452469fc5fa6e735b5508b02ff
Olena Sinelnik <kozebobinka@gmail.com>
2018-07-19 14:44:01 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <style>
+        html {
+            display: table;
+            height: 100%;
+            width: 100%;
+        }
+    
+        body {
+            display: table-cell;
+            vertical-align: middle;
+        }
+    
+        div {
+            margin: auto;
+            width: 100px;
+            height: 100px;
+            outline: inset 100px green;
+            outline-offset: -125px;
+        }
+    </style>
+</head>
+<body>
+    <div></div>
+</body>
+</html>
